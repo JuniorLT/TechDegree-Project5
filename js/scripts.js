c41289cf0a04762adef6b8c2300a0ecc5378a8b2
JuniorLT <Lam2@calcharter.org>
2020-01-11 18:17:37 -0800

    Updated generateCard() function
    Removed code that was making individual requests.
    
    Smoothly creates cards and appends it to the gallery.

--- js/scripts.js
+++ js/scripts.js
@@ -26,65 +26,68 @@ function getJSON(url, callback){
 
 // function that creates the HTML elements for the data and appends to the HTML doc
 function generateCard(data){
-  // creates an HTML element
-  const section = document.createElement('section');
-  // appends the element to the div
-  gallery.append(section);
-  // console.log(data);
-  // creates the card for each employee with summary of person
-  section.innerHTML = `
-  <div class="card">
-      <div class="card-img-container">
-          <img class="card-img" src="${data.results[0].picture.large}">
-      </div>
-      <div class="card-info-container">
-          <h3 id="name" class="card-name cap">${data.results[0].name.first + " " + data.results[0].name.last}</h3>
-          <p class="card-text">${data.results[0].email}</p>
-          <p class="card-text cap">${data.results[0].location.state + ", " + data.results[0].location.city}</p>
-      </div>
-  </div>
-  `;
+  data.results.map( person => {
+      // creates an HTML element
+      const galleryDiv = document.createElement('div');
 
-  // event listener that waits until card is clicked
-  section.addEventListener('click', (event) => {
-    // variables that hold the parts of the date of birth
-    var dob = data.results[0].dob.date;
-    var month = dob.substr(5, 2);
-    var day = dob.substr(8, 2);
-    var year = dob.substr(0, 4);
-
-    // variable that creates a div for the modalWindow
-    var modalWindow = document.createElement('div');
-
-    // adds the div to the body
-    document.body.append(modalWindow);
-    // creates the modal window with detailed info on person clicked
-    modalWindow.innerHTML = `
-      <div class="modal-container">
-          <div class="modal">
-              <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-              <div class="modal-info-container">
-                  <img class="modal-img" src="${data.results[0].picture.large}" alt="profile picture">
-                  <h3 id="name" class="modal-name cap">${data.results[0].name.first + " " + data.results[0].name.last}</h3>
-                  <p class="modal-text">${data.results[0].email}</p>
-                  <p class="modal-text cap">${data.results[0].location.city}</p>
-                  <hr>
-                  <p class="modal-text">${data.results[0].cell}</p>
-                  <p class="modal-text">${data.results[0].location.street.number+" "+data.results[0].location.street.name+" "+data.results[0].location.city+", "+data.results[0].location.state}</p>
-                  <p class="modal-text">Birthday: ${month+"/"+day+"/"+year}</p>
-              </div>
+      galleryDiv.classList.add("card");
+      // appends the element to the div
+      gallery.append(galleryDiv);
+      // console.log(data);
+      // creates the card for each employee with summary of person
+      galleryDiv.innerHTML = `
+          <div class="card-img-container">
+              <img class="card-img" src="${person.picture.large}">
+          </div>
+          <div class="card-info-container">
+              <h3 id="name" class="card-name cap">${person.name.first + " " + person.name.last}</h3>
+              <p class="card-text">${person.email}</p>
+              <p class="card-text cap">${person.location.state + ", " + person.location.city}</p>
           </div>
       `;
 
-      // variable that holds the x button
-      var xButton = document.getElementById("modal-close-btn");
-      // event listener that waits until x button is clicked
-      xButton.addEventListener('click', (button) => {
-        // removes the modal window
-        modalWindow.remove();
+      // event listener that waits until card is clicked
+      galleryDiv.addEventListener('click', (event) => {
+        // variables that hold the parts of the date of birth
+        var dob = person.dob.date;
+        var month = dob.substr(5, 2);
+        var day = dob.substr(8, 2);
+        var year = dob.substr(0, 4);
+
+        // variable that creates a div for the modalWindow
+        var modalWindow = document.createElement('div');
+
+        // adds the div to the body
+        document.body.append(modalWindow);
+        // creates the modal window with detailed info on person clicked
+        modalWindow.innerHTML = `
+          <div class="modal-container">
+              <div class="modal">
+                  <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                  <div class="modal-info-container">
+                      <img class="modal-img" src="${person.picture.large}" alt="profile picture">
+                      <h3 id="name" class="modal-name cap">${person.name.first + " " + person.name.last}</h3>
+                      <p class="modal-text">${person.email}</p>
+                      <p class="modal-text cap">${person.location.city}</p>
+                      <hr>
+                      <p class="modal-text">${person.cell}</p>
+                      <p class="modal-text">${person.location.street.number+" "+person.location.street.name+" "+person.location.city+", "+person.location.state}</p>
+                      <p class="modal-text">Birthday: ${month+"/"+day+"/"+year}</p>
+                  </div>
+              </div>
+          `;
+
+          // variable that holds the x button
+          var xButton = document.getElementById("modal-close-btn");
+          // event listener that waits until x button is clicked
+          xButton.addEventListener('click', (button) => {
+            // removes the modal window
+            modalWindow.remove();
+          });
       });
-  });
-};
+    });
+  };
+
 
 // gets the JSON from the API and calls back the generateHTML() function for the data
 // makes sure only 12 users are GET from the API
